4bc0438f98f42008e2b2d837758086ed58a38bb7
Liju <liju.raju@philips.com>
2024-06-16 22:28:17 +0530

    myGitRepo

---
+++ src/Lab002.java
@@ -0,0 +1,8 @@
+public class Lab002 {
+    public static void main(String[] args) {
+        int age = 19;
+        age = age +1;
+        System.out.println(age);
+
+    }
+}
---
+++ src/Lab003.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+        System.out.println("Enter a operator:");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // Handle division by zero
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+                return;
+        }
+        System.out.println("The result is :"+ result);
+
+      scanner.close();
+    }
+}
